c98df32d930f26223daf4c993998d789da482c11
carterkelsey <ckelsey27@students.stab.org>
2024-11-08 10:59:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-function getShoe(){
+function getShoe(weather, price){
 
     // the user inputs from the text boxes (saved in HTML) and save them to variables
-  var weather= document.getElementById("weather").value;
-  var price= document.getElementById("price").value; 
+  // var weather= document.getElementById("weather").value;
+  // var price= document.getElementById("price").value; 
   // console.log(weather);
   // console.log(price);
-  // log to make sure our code is not broken in any way
+  // log to make sure our code is not broken in any way and variables we changed into parameters that we could call back later
   
     //use variables to determine outputs
   var shoe= "Choose Shoe";
@@ -27,48 +26,52 @@ function getShoe(){
     console.log(shoe)
     return shoe
 }
+// calling return value function
 function displayImage(){
-  var QR= 1
-  if (shoe= "Fancy Sneaker"){
-   QR= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nike.com%2Fw%2F100-and-under-running-shoes-37v7jz3s37kzy7ok&psig=AOvVaw30uGWl92DnYh-ahNAYlsAO&ust=1731074551578000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjJ8K-xyokDFQAAAAAdAAAAABAR"
+  var shoe = getShoe(document.getElementById("weather").value,document.getElementById("price").value);
+  var shoePic;
+  if (shoe== "Fancy Sneaker"){
+    shoePic= "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/17d94aa9-1578-4faf-b989-73c0efc580b2/W+NIKE+JOURNEY+RUN.png"
+
   }
-  else if (shoe= "Fancy Rainboot"){
-    QR= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FHunter-Womens-Play-Short-Backstrap%2Fdp%2FB0CJWCQ29W&psig=AOvVaw0hWn0gEWKrUOrMdtwb3WxK&ust=1731074428404000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJj18P2wyokDFQAAAAAdAAAAABAK"
+  else if (shoe== "Fancy Rainboot"){
+    shoePic= "https://m.media-amazon.com/images/I/5154sRYic-L._AC_SL1500_.jpg"
   }
 
-   else if (shoe= "Cheaper Sneaker"){
-    QR= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2F284744250459&psig=AOvVaw2amYmGv0i2cDjxgANd97F7&ust=1731074578325000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNC068CxyokDFQAAAAAdAAAAABAE"
+   else if (shoe== "Cheaper Sneaker"){
+    shoePic= "https://vader-prod.s3.amazonaws.com/1711659300-salomonthundercross-6605d898e1fb2.jpg"
+
    }
-  else if (shoe= "Cheaper Rainboot"){
-    QR= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FWTW-Waterproof-Neoprene-Outdoor-Durable%2Fdp%2FB08ZQVBNHX&psig=AOvVaw2vEMVRcbnTN-TKMQEeaEZF&ust=1731074519659000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICb-KGxyokDFQAAAAAdAAAAABAJ"
-   }
+  else if (shoe== "Cheaper Rainboot"){
+    shoePic= "https://m.media-amazon.com/images/I/51lvyikppYL._AC_UY900_.jpg"
+
+  }
+document.getElementById("images").setAttribute("src", shoePic)
+  getComment(shoe)
+
   }
 
-
-  function getComment(string1,string2,string3){
+  function getComment(chosenShoe){
   
-    var feedback=(string1.length, string2.length, string3.length);
-    
-    if (shoe=="Fancy Sneaker"){
-        return string1;
+    var feedback;
+    if (chosenShoe=="Fancy Sneaker"){
+        feedback = "Nike is a good choice";
     }
-    if (shoe=="Fancy Rainboot"){
-      return string2;
+    if (chosenShoe=="Fancy Rainboot"){
+      feedback = "Hunter boots are long lasting";
     }
-    if (shoe=="Cheap Sneaker"){
-      return string3;
+    if (chosenShoe=="Cheaper Sneaker"){
+      feedback = "affordable and fasionable!";
     }
-    if (shoe=="Cheap Rainboot"){
-      return string4;
+    if (chosenShoe=="Cheaper Rainboot"){
+      feedback = "Great rain boot for a good cost";
 
-    }
-      
-    console.log(getComment("Nike is a good choice", "Hunter boots are long lasting", "affordable and fasionable!", "Great rain boot for a good cost"));
-    document.getElementById("feedback").innerHTML = feedback;
+    }   
     
+    document.getElementById("feedback").innerHTML = feedback;
+  }
 
   
     //Lastly, set the results to display on the screen. Here is an example, although this will just set the results to be an empty string (you'll need to edit what it equals).
   
-    
-  }
+
